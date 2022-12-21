d1aa5b26eae9445dd084c32b7b9e184f5141dc95
rushba1911 <121183841+rushba1911@users.noreply.github.com>
2022-12-21 08:47:55 -0800

    Add files via upload

---
+++ adm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"*******************************************************"<<endl;
+cout<<"*                                                     *"<<endl;
+cout<<"*                                                     *"<<endl;
+cout<<"*                UNIVERSITY ADMISSION                 *"<<endl;
+cout<<"*                 MANAGEMENT SYSTEM                   *"<<endl;
+cout<<"*                                                     *"<<endl;
+cout<<"*                                                     *"<<endl;
+cout<<"*******************************************************"<<endl;
+}
