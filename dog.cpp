e68815318f1f93d4c61a8b5fa79ce35e6669a8e2
rushba1911 <121183841+rushba1911@users.noreply.github.com>
2022-12-24 03:24:51 -0800

    Add files via upload

---
+++ dog.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+cout<<"-----------------------------------------------------"<<endl;
+cout<<"           O    ^ ____ ^                             "<<endl;
+cout<<"            O   (  oo  ) \\ __________                "<<endl;
+cout<<"                (  __  ) \\          )\\/\\            "<<endl;
+cout<<"                         ||-------w  |               "<<endl;
+cout<<"                         ||         ||               "<<endl;
+}
